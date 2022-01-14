908b93890b22be6e7350a8d55af0e94bd0952c8d
hwakk <napia1014@gmail.com>
2022-01-14 18:38:12 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {Routes, Route, NavLink, useParams, BrowserRouter} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home
+    </div>
+  )
+}
+
+let contents = [
+  {id:1, title:'HTML', description:'HTML is ...'},
+  {id:2, title:'JS', description:'JS is ...'},
+  {id:3, title:'React', description:'React is ...'}
+]
+
+function Topic() {
+  let params = useParams();
+  let topic_id = params.topic_id;
+  let selected_topic = {
+    title:'None',
+    description: 'Not Found'
+  }
+  for(let i = 0; i < contents.length; i++) {
+    if(contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
+function Topics() {
+  let list = [];
+  for(let i = 0; i < contents.length; i++) {
+    list.push(<li key={contents[i].id}><NavLink to={'/topics/' + contents[i].id}>{contents[i].title}</NavLink></li>)
+  }
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {list}
+      </ul>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <h1>React Router DOM example</h1>
+      <ul>
+        <li><NavLink to={'/'}>Home</NavLink></li>
+        <li><NavLink to={'/topics'}>Topics</NavLink></li>
+        <li><NavLink to={'/contact'}>Contact</NavLink></li>
+      </ul>
+      <Routes>
+        <Route path='/topics/:topic_id' element={<Topic></Topic>}></Route>
+      
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/topics" element={<Topics></Topics>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
